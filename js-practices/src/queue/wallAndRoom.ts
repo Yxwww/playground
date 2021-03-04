c2315cc65e30e4b9c550c338a8721beba5933cd8
Yuxi Wang <yuxi.wangcd@gmail.com>
2021-03-04 15:35:39 -0700

    Add steps counting for one room

--- js-practices/src/queue/wallAndRoom.ts
+++ js-practices/src/queue/wallAndRoom.ts
@@ -1,13 +1,5 @@
 import { createQueue } from '../circular-queue.ts'
-function isEmpty(room: number) {
-  return room === 2147483647
-}
-function isGate(room: number) {
-  return room === 0
-}
-function isWall(room: number) {
-  return room === -1
-}
+
 const rooms = [
   [2147483647, -1, 0, 2147483647],
   [2147483647, 2147483647, 2147483647, -1],
@@ -17,11 +9,20 @@ const rooms = [
 const result = [[3, -1, 0, 1], [2, 2, 1, -1], [1, -1, 2, -1], [0, -1, 3, 4]]
 
 type Vec2 = [number, number]
+function isEmpty(room: number) {
+  return room === 2147483647
+}
+function isGate(room: number) {
+  return room === 0
+}
+function isWall(room: number) {
+  return room === -1
+}
 
-function createNode(room: number, coord: Vec2) {
+function createNode(room: number, coord: Vec2, steps = 10000) {
   return {
     room,
-    distance: room,
+    distance: steps,
     coord,
   }
 }
@@ -34,9 +35,13 @@ function inRange(v: number, [min, max]: Vec2) {
   return v < max && v >= min
 }
 
-function getNeighbours(rooms: number[][], currentCoord: Vec2): WNode[] {
+function getNeighbours(
+  rooms: number[][],
+  currentCoord: Vec2,
+  steps: number
+): WNode[] {
   const neighbours: WNode[] = []
-  const [curY, curX] = currentCoord
+  const [curX, curY] = currentCoord
   const m = rooms[0].length
   const n = rooms.length
   function isXInRange(x: number) {
@@ -46,33 +51,33 @@ function getNeighbours(rooms: number[][], currentCoord: Vec2): WNode[] {
     return inRange(y, [0, n])
   }
   function pushNode(x: number, y: number) {
-    neighbours.push(createNode(rooms[y][x], [y, x]))
+    const room = rooms[y][x]
+    neighbours.push(createNode(room, [x, y], steps))
   }
 
   const leftX = curX - 1
   const leftY = curY
-  if (isXInRange(leftX) && isYInRange(leftY) && !visited[`${leftY}${leftX}`]) {
+  if (isXInRange(leftX) && isYInRange(leftY) && !visited[`${leftX}${leftY}`]) {
     pushNode(leftX, leftY)
   }
   const rightX = curX + 1
   const rightY = curY
-  console.log({ rightX, rightY })
   if (
-    !visited[`${rightY}${rightX}`] &&
+    !visited[`${rightX}${rightY}`] &&
     isXInRange(rightX) &&
     isYInRange(rightY)
   ) {
     pushNode(rightX, rightY)
   }
   const topX = curX
-  const topY = curY + 1
-  if (!visited[`${topY}${topX}`] && isXInRange(topX) && isYInRange(topY)) {
+  const topY = curY - 1
+  if (!visited[`${topX}${topY}`] && isXInRange(topX) && isYInRange(topY)) {
     pushNode(topX, topY)
   }
   const bottomX = curX
-  const bottomY = curY - 1
+  const bottomY = curY + 1
   if (
-    !visited[`${bottomY}${bottomX}`] &&
+    !visited[`${bottomX}${bottomY}`] &&
     isXInRange(bottomX) &&
     isYInRange(bottomY)
   ) {
@@ -83,13 +88,24 @@ function getNeighbours(rooms: number[][], currentCoord: Vec2): WNode[] {
 
 function wallsAndGates(rooms: number[][]) {
   const queue = createQueue<WNode>(100)
-  queue.enqueue(createNode(rooms[0][0], [0, 0]))
+  queue.enqueue(createNode(rooms[0][0], [0, 0], 0))
   let current = queue.dequeue()
+  let steps = 0
   while (current) {
     visited[current.coord.join('')] = current
-    const neighbours = getNeighbours(rooms, current.coord)
-    console.log('neighbours', neighbours)
-    neighbours.forEach(n => {
+    steps++
+    const neighbours = getNeighbours(rooms, current.coord, steps)
+    const gateNeighbours = neighbours.filter(n => isGate(n.room))
+    const emptyNeighbours = neighbours.filter(n => isEmpty(n.room))
+    if (gateNeighbours.length > 0) {
+      console.log('found gate near', current.coord)
+      Object.entries(visited).forEach(([coord, n]) => {
+        console.log(coord, '-', n.distance)
+        n.distance = steps - n.distance
+      })
+      break
+    }
+    emptyNeighbours.forEach(n => {
       queue.enqueue(n)
     })
     // const neighbours = ;
@@ -98,4 +114,7 @@ function wallsAndGates(rooms: number[][]) {
 }
 
 wallsAndGates(rooms)
-console.log(`visited ${Object.keys(visited).length}`, visited)
+console.log(
+  `visited ${Object.keys(visited).length}`,
+  Object.entries(visited).map(([coord, { distance }]) => ({ coord, distance }))
+)
