40547ff4be0aba97dd92dbe99058ec1fa8d86e2c
Yuxi Wang <yuxi.wangcd@gmail.com>
2023-05-29 15:01:13 -0700

    Update eslint config
    - format stack.js

--- js-practices/src/leetcode/stack.js
+++ js-practices/src/leetcode/stack.js
@@ -1,46 +1,45 @@
-const assert = require('assert')
+const assert = require("assert");
 
-
-const OPEN_TAG = '[({';
-const CLOSE_TAG = '])}';
+const OPEN_TAG = "[({";
+const CLOSE_TAG = "])}";
 
 function isOpenTag(c) {
-  return OPEN_TAG.indexOf(c) !== -1
+  return OPEN_TAG.indexOf(c) !== -1;
 }
 
 function isCloseTag(c) {
-  return CLOSE_TAG.indexOf(c) !== -1
+  return CLOSE_TAG.indexOf(c) !== -1;
 }
 
 /**
-* @param {string} c
-* @returns {string}
-*/
+ * @param {string} c
+ * @returns {string}
+ */
 function getMatchCloseTag(c) {
   const index = OPEN_TAG.indexOf(c);
   if (index === -1) {
-    throw new Error('character is not a tag', c);
+    throw new Error("character is not a tag", c);
   }
 
   return CLOSE_TAG[index];
 }
 
 /**
-  * @param {Array<string>} - arr
-  * @returns string
-  */
+ * @param {Array<string>} - arr
+ * @returns string
+ */
 function peek(arr) {
-  return arr[arr.length - 1]
+  return arr[arr.length - 1];
 }
 
 /**
-  * @param {string} input
-  * @returns {boolean}
-  */
+ * @param {string} input
+ * @returns {boolean}
+ */
 function checkParentheses(input) {
   const stack = [];
   for (let i = 0; i < input.length; i++) {
-    const c = input[i]
+    const c = input[i];
     // If(isOpenTag(c) || isCloseTag(c)) {
     //   stack.push()
     // }
@@ -48,26 +47,25 @@ function checkParentheses(input) {
       stack.push(c);
     } else if (isCloseTag(c)) {
       const last = peek(stack);
-      console.log('last', { last, c, stack })
+      console.log("last", { last, c, stack });
       if (!isOpenTag(last)) {
-        return false
+        return false;
       }
 
       if (getMatchCloseTag(last) !== c) {
-        return false
+        return false;
       }
 
-      console.log('popping', last)
+      console.log("popping", last);
 
       stack.pop();
     }
   }
 
-  console.log('stack', stack, stack.length === 0);
-  return stack.length === 0
-
+  console.log("stack", stack, stack.length === 0);
+  return stack.length === 0;
 }
 
-assert.equal(checkParentheses('{1'), false)
-assert.equal(checkParentheses('[{1}]'), true)
-assert.equal(checkParentheses('[{1}]}'), false)
+assert.equal(checkParentheses("{1"), false);
+assert.equal(checkParentheses("[{1}]"), true);
+assert.equal(checkParentheses("[{1}]}"), false);
